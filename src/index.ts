a7277a769046df9c5dc8a56a549e762ab83c87ce
reinvheels <reinvheels@gmail.com>
2024-08-25 16:43:35 +0200

    first operation traversal

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,19 @@ import type { OpenAPIV3_1 as oas } from 'openapi-types';
 
 const spec = JSON.parse(fs.readFileSync('examples/petstore.json').toString()) as oas.Document;
 
-console.log(spec.info);
+fs.writeFileSync('examples/petstore.html', '');
+
+spec.paths &&
+    Object.entries(spec.paths).forEach(([path, pathObject]) => {
+        pathObject &&
+            Object.entries(pathObject).forEach(([method, _operation]) => {
+                const operation = _operation as oas.OperationObject;
+                fs.appendFileSync(
+                    'examples/petstore.html',
+                    /*html*/ `<div>
+                <h2>${operation.operationId}</h2>
+                <p>${operation.description}</p>
+            </div>`,
+                );
+            });
+    });
